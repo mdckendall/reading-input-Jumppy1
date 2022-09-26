85b6fdb22b14ab244d76b7afdabd7da06303beb6
Jumppy1 <113559416+Jumppy1@users.noreply.github.com>
2022-09-26 08:18:30 +0000

    i tried doing it this way

--- Main.java
+++ Main.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter a word:");
-
-    Scanner myScanner = new Scanner(System.in);
+   
+    
+       Scanner myScanner = new Scanner(System.in);
     String UserInput = myScanner.next();
-    System.out.println (UserInput);
+    
+    System.out.println("Enter a word: " + UserInput);
+
+   
   
   }
 }
