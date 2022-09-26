93be00e94bf4c944e9cc91b08ee76679f42f8b5d
Jumppy1 <113559416+Jumppy1@users.noreply.github.com>
2022-09-26 08:31:57 +0000

    i inserted a input

--- Main.java
+++ Main.java
@@ -4,12 +4,13 @@ class Main {
   public static void main(String[] args) {
    
     
+    
+    System.out.println("Enter a word: ");
+
     Scanner myScanner = new Scanner(System.in);
     String UserInput = myScanner.next();
-    
-    System.out.println("Enter a word: " + UserInput);
 
-   
-
+    System.out.println(UserInput);
+  
   }
 }
