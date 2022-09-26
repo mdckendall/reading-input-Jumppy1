048354a57fc80e7d98fab1adc480aa20268d8aa4
Jumppy1 <113559416+Jumppy1@users.noreply.github.com>
2022-09-26 08:28:04 +0000

    i put a input with a output giving me the result i want

--- Main.java
+++ Main.java
@@ -4,14 +4,12 @@ class Main {
   public static void main(String[] args) {
    
     
-    
-    
-    System.out.println("Enter a word: ");
-
-   Scanner myScanner = new Scanner(System.in);
+    Scanner myScanner = new Scanner(System.in);
     String UserInput = myScanner.next();
+    
+    System.out.println("Enter a word: " + UserInput);
 
+   
 
-    System.out.println ("Enter a word: " + UserInput);
   }
 }
